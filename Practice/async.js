0954cf227b412d8a075895c5173e5792b73f556f
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-08-17 06:34:03 +0600

    chore: Add timeout event handling to async code

--- Practice/async.js
+++ Practice/async.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 
 var EventEmitter = require('events');
+const { time } = require('console');
 
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on('Program Ended', () => {
     console.log('Event Handler Called');
+});
+
+eventEmitter.on('Timeout Called', ({ timeout, text}) => {
+    console.log(`It Was Called After ${timeout} Seconds ${text}`);
 });
 
 fs.readFile('example.txt', (err, data) => {
@@ -15,5 +20,11 @@ fs.readFile('example.txt', (err, data) => {
     console.log(data.toString());
     setTimeout(() => {
         eventEmitter.emit('Program Ended');
+    }, 1000);
+    setTimeout(() => {
+        eventEmitter.emit('Timeout Called', {
+            timeout: 2000,
+            text: 'Later'
+        });
     }, 2000);
 });
