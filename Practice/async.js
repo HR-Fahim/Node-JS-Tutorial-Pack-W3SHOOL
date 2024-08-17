91b542cdda1e1cce5b6f1c9b60d601b9ce034802
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-08-17 06:22:13 +0600

    chore: Add event handling to async code

--- Practice/async.js
+++ Practice/async.js
@@ -1,10 +1,17 @@
 var fs = require('fs');
 
+var EventEmitter = require('events');
+
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('Program Ended', () => {
+    console.log('Event Handler Called');
+});
+
 fs.readFile('example.txt', (err, data) => {
     if (err) {
         return console.error(err);
     }
     console.log(data.toString());
+    eventEmitter.emit('Program Ended');
 });
-
-console.log('Program Ended');
